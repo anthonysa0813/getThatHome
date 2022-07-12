a93c9308c6e50e251572a481de041b78f818160a
anthony <anthonysa0813@gmail.com>
2022-07-12 13:21:19 -0500

    system project

---
+++ src/pages/landingPage/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HeaderContainer } from "../styles/header";
+
+const Header = () => {
+  return <HeaderContainer>Header</HeaderContainer>;
+};
+
+export default Header;
