8c42269365c414d468413f1acde93284d13a172d
Jhoksan Socla <jhoksansaid@gmail.com>
2022-07-13 19:16:25 -0500

    ADD: Global, Colors & Typography Styles

---
+++ src/pages/landingPage/styles/typography.js
@@ -0,0 +1,59 @@
+export const fonts = {
+  primary: `"Montserrat", -apple-system, "Helvetica Neue", sans-serif`,
+  secondary: `"Inter", -apple-system, "Helvetica Neue", sans-serif`,
+};
+
+export const typography = {
+  text: {
+    xs: `
+    font-size: 0.625rem;
+    line-height: 1rem;
+    `,
+    s: `
+    font-size: 0.75rem;
+    line-height: 1rem;
+    `,
+    sm: `
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    `,
+    md: `
+    font-size: 1rem;
+    line-height: 1.5rem;
+    `,
+    lg: `
+    font-size: 1.375rem;
+    line-height: 1.75rem;
+    `,
+    xl: `
+    font-size: 1.75rem;
+    line-height: 2.2rem;
+    `,
+  },
+  head: {
+    xs: `
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+    `,
+    s: `
+    font-size: 1.5rem;
+    line-height: 2rem;
+    `,
+    sm: `
+    font-size: 2.25rem;
+    line-height: 3rem;
+    `,
+    md: `
+    font-size: 3rem;
+    line-height: 4rem;
+    `,
+    lg: `
+    font-size: 4rem;
+    line-height: 5.5rem;
+    `,
+    xl: `
+    font-size: 6rem;
+    line-height: 8rem;
+    `,
+  },
+};
