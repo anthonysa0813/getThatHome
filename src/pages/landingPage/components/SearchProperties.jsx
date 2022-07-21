760c26334403a6e63035f1e7e3d398496ff55518
anthony <anthonysa0813@gmail.com>
2022-07-21 17:49:09 -0500

    some changes

--- src/pages/landingPage/components/SearchProperties.jsx
+++ src/pages/landingPage/components/SearchProperties.jsx
@@ -12,32 +12,28 @@ const SearchProperties = ({ setShowModal, showModal }) => {
   const [properties, setProperties] = useState([]);
   const { propertiesArr, setPropertiesArr } = useContext(PropertyContext);
   const [filters, setFilters] = useState({
-      minPrice: 1000,
-      maxPrice: 2000,
-      property_type: ["house", "apartment"],
-      beds: 4,
-      baths: 2,
-      pets: true,
-      minArea: 100,
-      maxArea: 200
-  })
-
-
-
-
+    minPrice: 1000,
+    maxPrice: 2000,
+    property_type: ["house", "apartment"],
+    beds: 4,
+    baths: 2,
+    pets: true,
+    minArea: 100,
+    maxArea: 200,
+  });
 
   useEffect(() => {
     getApi("properties").then((res) => {
       setPropertiesArr(res);
       setProperties(res);
-      console.log(res)
+      console.log(res);
     });
   }, [filters]);
 
   return (
     <>
       <div className="wrapper">
-        <Filters filters={filters} setFilters={setFilters}/>
+        <Filters filters={filters} setFilters={setFilters} />
         <CardContainer className="cardContainer mt-1 ">
           {properties.map((property, index) => {
             if (
@@ -49,10 +45,9 @@ const SearchProperties = ({ setShowModal, showModal }) => {
               property.area >= filters.minArea &&
               property.area <= filters.maxArea &&
               filters.property_type.includes(property.property_type)
-              ){
+            ) {
               return <CardBox key={index} property={property} />;
             }
-
           })}
         </CardContainer>
       </div>
